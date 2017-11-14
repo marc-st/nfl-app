ca040dd00f0001d70854986b0e1d389e00fcdc08
marc-st <marc.stevens99@gmail.com>
2017-11-14 17:13:35 +0000

    added initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+first = Game.where(home: 'Home Team').first
+first.delete if first
+
+games = Game.create([home: 'Home Team', homescore: 0, away: 'Away Team', awayscore: 0, date: "00-00-0000", league: 'League'])
