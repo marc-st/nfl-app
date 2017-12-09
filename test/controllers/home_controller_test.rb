5ee4c0f04a4c3f68e9191ea1753b56fe05274d43
marc-st <marc.stevens99@gmail.com>
2017-12-09 19:44:35 +0000

    tested Home controller and view

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  
+  # /home route success and test home view
+  test "should get home" do
+    get :home
+    assert_response :success
+    
+    assert_template layout: 'application'
+    
+    assert_select 'title', 'NFL Web App'
+    assert_select 'h1', 'NFL Home Page'
+    assert_select 'p', 'Author Marc Stevens'
+  end
+  
+  # /contact route success and test contact view
   test "should get contact" do
     get :contact
     assert_response :success
@@ -10,29 +24,18 @@ class HomeControllerTest < ActionController::TestCase
     assert_select 'title','NFL Web App'
     assert_select 'h1', 'Contact Us'
     assert_select 'p', 'Complete the following form to get in touch with us.'
-    
-  end
-  test "should get home" do
-    get :home
-    assert_response :success
-    
-    assert_template layout: 'application'
-    
-    assert_select 'title', 'NFL Web App'
-    assert_select 'h1', 'Home Page'
-    assert_select 'p', 'Welcome to the homepage of the NFL Web App'
-    
   end
   
+  # /request_contact post success with flash notice (no parameters)
   test "should post request contact but no email" do
     post :request_contact
     
     assert_response :redirect
     assert_not_empty flash[:alert]
     assert_nil flash[:notice]
-    
   end
   
+  # /request_contact post success with flash notice (with parameters)
   test "should post request contact" do
     post :request_contact, name: "Marc", 
     email: "me@email.com", telephone: "1234567",
@@ -41,7 +44,6 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_not_empty flash[:notice]
     assert_nil flash[:alert]
-    
   end  
 
 end
